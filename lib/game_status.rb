d864a6124ccdbadc28137649d8d437e61f77b8ae
Benjamin Stein <whereyouben@gmail.com>
2020-09-14 05:29:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,8 +8,9 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
+  #iterate over the possible wins
   WIN_COMBINATIONS.each do |win_option|
-    #if the first position of the line is not empty
+    #if the first position of the winning possibility is filled
     if position_taken?(board, win_option[0])
       #and if the second and third positions in the line are the same
       if board[win_option[0]] == board[win_option[1]] && board[win_option[0]] == board[win_option[2]]
