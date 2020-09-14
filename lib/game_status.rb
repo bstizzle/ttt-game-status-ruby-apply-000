57acb9a062598b183847d3d4450dac63b92fe096
Benjamin Stein <whereyouben@gmail.com>
2020-09-14 05:26:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,11 +8,7 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  counter = 0 
-  #there are only 8 possible winning lines in tictactoe, so hard code attempts to 8
-  while counter < 8
-    #set a variable for the winning line for each iteration
-    win_option = WIN_COMBINATIONS[counter]
+  WIN_COMBINATIONS.each
     #if the first position of the line is not empty
     if position_taken?(board, win_option[0])
       #and if the second and third positions in the line are the same
