c4725709ea3dc75e9c0176639c9595e889d114bc
Benjamin Stein <whereyouben@gmail.com>
2020-09-14 06:03:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,11 +24,9 @@ def won?(board)
 end
 
 def full?(board)
-  full_board == |index|
+  full_board = board.all? do |index|
     position_taken?(board, index)
   end 
-  #otherwise, return true
-  return true 
 end 
 
 def draw?(board)
