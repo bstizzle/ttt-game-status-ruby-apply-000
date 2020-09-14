74eaabea21f5ce78d576b928c2abb159ae79b8de
Benjamin Stein <whereyouben@gmail.com>
2020-09-14 04:47:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,9 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  board.select do
-    if     
+  counter = 0 
+  while counter < 8
+    win_option = WIN_COMBINATIONS[counter]
+    
   end
 end
